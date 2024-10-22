2d7857e74a76bfc70d3771c3ae9580040fe4bcd4
Luis Dardon <162521043+ilDardon@users.noreply.github.com>
2024-10-22 13:12:37 -0600

    Update app.js

--- dev/app.js
+++ dev/app.js
@@ -26,6 +26,7 @@ class App{
         this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
         this.renderer.setPixelRatio( window.devicePixelRatio );
         this.renderer.setSize( window.innerWidth, window.innerHeight );
+	    this.renderer.outputEncoding = THREE.sRGBEncoding;
         container.appendChild( this.renderer.domElement );
 
         // Controles de la cámara
