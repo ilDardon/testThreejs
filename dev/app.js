291dbc69d4cad7ae60bb71c6ca2f41d43576b0a3
Luis Dardon <162521043+ilDardon@users.noreply.github.com>
2024-10-23 13:19:46 -0600

    Update app.js

--- dev/app.js
+++ dev/app.js
@@ -38,8 +38,8 @@ class App {
     initScene() {
         this.geometries = [
             new THREE.BoxBufferGeometry(0.1, 0.1, 0.1),
-            new THREE.SphereBufferGeometry(0.1, 48, 48),
-            new THREE.ConeBufferGeometry(0.1, 0.15, 4, 32)
+            new THREE.SphereBufferGeometry(0.1, 64, 64),
+            new THREE.ConeBufferGeometry(0.05, 0.1, 64, 64)
         ];
         this.meshes = [];
     }
